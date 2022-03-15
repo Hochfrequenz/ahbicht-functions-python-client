3c77ea56274702aa2e0b87b7e86e286d9e82d34e
konstantin <konstantin.klein@hochfrequenz.de>
2022-03-15 17:11:50 +0100

    🎉Move Code From AHBicht into a Package📦 (#1)
    Co-authored-by: Annika <73470827+hf-aschloegl@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
-# This file is only needed for python packages
+from setuptools import setup
 
-# from setuptools import setup
-
-# setup(use_scm_version=True)
+setup(name="ahbichtfunctionsclient", use_scm_version=True)
