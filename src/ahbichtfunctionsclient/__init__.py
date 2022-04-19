33df00bb1d6fb15862af1708dc0dc906958ddfd3
HF-FSteinkamp <frederik.steinkamp@hochfrequenz.de>
2022-04-19 17:46:09 +0200

    :sparkles: Add ConditionKey Resolver :white_check_mark:

--- src/ahbichtfunctionsclient/__init__.py
+++ src/ahbichtfunctionsclient/__init__.py
@@ -4,13 +4,19 @@ condition expression or a condition text for a given package/condition key.
 """
 import aiohttp
 from ahbicht.expressions.package_expansion import PackageResolver
-from ahbicht.mapping_results import PackageKeyConditionExpressionMapping, PackageKeyConditionExpressionMappingSchema
+from ahbicht.mapping_results import (
+    ConditionKeyConditionTextMapping,
+    ConditionKeyConditionTextMappingSchema,
+    PackageKeyConditionExpressionMapping,
+    PackageKeyConditionExpressionMappingSchema,
+)
+from maus.edifact import EdifactFormat, EdifactFormatVersion
 
 
 # pylint: disable=too-few-public-methods
 class HochfrequenzPackageResolver(PackageResolver):
     """
-    A package resolver that uses a REST API (by Hochfrequenz) to resolve packages.
+    A package resolver that uses a REST API (by Hochfrequenz) to request a condition expression for a given package key.
     Note that this resolver requires an internet connection to work and the Hochfrequenz API to be up and running.
     Consider using this resolver to retrieve package information once and then dump them into something fast and stable
     like e.g. a JSON file, a database or feed its results into a hardcoded package resolver once on startup.
@@ -36,4 +42,47 @@ class HochfrequenzPackageResolver(PackageResolver):
                     )
                 response_body = await response.json()
                 result = PackageKeyConditionExpressionMappingSchema().load(response_body)
+                return result
+
+
+# pylint: disable=too-few-public-methods
+class HochfrequenzConditionResolver:
+    """
+    A package resolver that uses a REST API (by Hochfrequenz) to request a condition text for a given condition key.
+    Note that this resolver requires an internet connection to work and the Hochfrequenz API to be up and running.
+    Consider using this resolver to retrieve package information once and then dump them into something fast and stable
+    like e.g. a JSON file, a database or feed its results into a hardcoded package resolver once on startup.
+    Relying on external web services is prone to be a bottleneck for your application.
+    """
+
+    _hochfrequenz_base_condition_uri = "https://ahbicht.azurewebsites.net/api/ResolveConditionText/"
+
+    def __init__(
+        self,
+        edifact_format_version: EdifactFormatVersion,
+        edifact_format: EdifactFormat,
+        api_url=_hochfrequenz_base_condition_uri,
+    ):
+        """
+        initializes the condition resolver and collect the edifact format as well as the version of the format.
+        """
+        self.api_url = api_url
+        self.edifact_format = edifact_format
+        self.edifact_format_version = edifact_format_version
+
+    async def get_condition_text(self, condition_key: str) -> ConditionKeyConditionTextMapping:
+        """
+        :param condition_key:
+        :return: ConditionKeyConditionTextMapping
+        """
+        request_url = f"{self.api_url}/{self.edifact_format_version}/{self.edifact_format}/{condition_key}"
+        async with aiohttp.ClientSession() as session:
+            async with session.get(request_url) as response:
+                print("Status:", response.status)
+                if response.status != 200:
+                    return ConditionKeyConditionTextMapping(
+                        condition_key=condition_key, condition_text=None, edifact_format=self.edifact_format
+                    )
+                response_body = await response.json()
+                result = ConditionKeyConditionTextMappingSchema().load(response_body)
                 return result
